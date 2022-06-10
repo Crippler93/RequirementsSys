62257ad94cf6540d2c109c2078745d49d442bc59
Crippler93 <luischavezcald@gmail.com>
2022-06-10 08:38:17 -0400

    removed unused components

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -8,8 +8,6 @@ import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { FormTypeComponent } from './form-type/form-type.component';
 import { TypeListComponent } from './type-list/type-list.component';
 import { typeReducer } from './reducers/type.reducer';
@@ -21,10 +19,8 @@ import { TypeEffect } from './effects/type.effect';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     FormTypeComponent,
-    TypeListComponent
+    TypeListComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -32,15 +28,13 @@ import { TypeEffect } from './effects/type.effect';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'type/new', component: FormTypeComponent },
       { path: 'type/list', component: TypeListComponent },
     ]),
-    StoreModule.forRoot({ types: typeReducer}),
-    EffectsModule.forRoot([TypeEffect])
+    StoreModule.forRoot({ types: typeReducer }),
+    EffectsModule.forRoot([TypeEffect]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
