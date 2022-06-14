69a59e488d12857e325ccafe95156949fdecfbf2
Luis Chavez <luischavezcald@gmail.com>
2022-06-14 16:21:56 -0400

    removed unused component

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -6,21 +6,22 @@ import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
 import { typeReducer } from './type/state/type.reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { TypeEffect } from './type/state/type.effect';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppRoutingModule } from './app.routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [AppComponent, NavMenuComponent, HomeComponent],
+  declarations: [AppComponent, NavMenuComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     ReactiveFormsModule,
+    RouterModule,
     AppRoutingModule,
-    StoreModule.forRoot({ type: typeReducer}),
+    StoreModule.forRoot({ type: typeReducer }),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
       autoPause: true,
--- ClientApp/src/app/app.routing.module.ts
+++ ClientApp/src/app/app.routing.module.ts
@@ -1,22 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Route, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 
 const routes: Route[] = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '', redirectTo: 'requirement', pathMatch: 'full' },
   {
     path: 'type',
     loadChildren: () =>
       import('./type/type.module').then((m) => m.TypeModule),
   },
   { path: 'requirement', loadChildren: () => import('./requirements/requirements.module').then(m => m.RequirementsModule) },
+  { path: '**', redirectTo: 'requirement'}
 ]
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule,
     RouterModule.forRoot(routes),
   ]
 })
